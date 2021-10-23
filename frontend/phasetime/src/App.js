311273914fdb09929c269b71525d84fdf1598c85
Brian <brianzheng927@gmail.com>
2021-10-23 00:38:01 -0700

    added grid

--- frontend/phasetime/src/App.js
+++ frontend/phasetime/src/App.js
@@ -2,13 +2,34 @@ import './App.css';
 import MyClass from './MyClass.js';
 import SearchBar from './SearchBar.js'
 import ClassList from './ClassList.js'
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 function App() {
   return (
     <div className="App">
       phase time
       <SearchBar />
-      <MyClass />
-//      <ClassList />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item m={12}>
+            <Item> <ClassList /></Item>
+          </Grid>
+          <Grid item m={12}>
+            <Item><MyClass /></Item>
+          </Grid>
+
+        </Grid>
+    </Box>
     </div>
   );
 }
--- frontend/phasetime/src/ClassList.js
+++ frontend/phasetime/src/ClassList.js
@@ -6,6 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
 import './App.css';
 
 function createData(name, unit, reserved, gradReq) {
@@ -25,7 +26,8 @@ function ClassList() {
       <Table sx={{ minWidth: 300}} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Class Name </TableCell>
+            <TableCell align="center">select </TableCell>
+            <TableCell align="center">Class Name </TableCell>
             <TableCell align="center">unit</TableCell>
             <TableCell align="center">reserved</TableCell>
             <TableCell align="center">gradReq</TableCell>
@@ -36,10 +38,8 @@ function ClassList() {
             <TableRow
               key={row.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
+            > <Checkbox defaultChecked />
+              <TableCell align="center" component="th" scope="row"> {row.name}</TableCell>
               <TableCell align="center">{row.unit}</TableCell>
               <TableCell align="center">{row.reserved}</TableCell>
               <TableCell align="center">{row.gradReq}</TableCell>
