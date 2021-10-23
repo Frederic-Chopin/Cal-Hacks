53fdbfcff73f79fed3da0bed255658c1401bdeec
Brian <brianzheng927@gmail.com>
2021-10-23 00:21:56 -0700

    class list added

--- frontend/phasetime/src/App.js
+++ frontend/phasetime/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import MyClass from './MyClass.js';
 import SearchBar from './SearchBar.js'
+import ClassList from './ClassList.js'
 function App() {
   return (
     <div className="App">
       phase time
       <SearchBar />
       <MyClass />
+//      <ClassList />
     </div>
   );
 }
---
+++ frontend/phasetime/src/ClassList.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import './App.css';
+
+function createData(name, unit, reserved, gradReq) {
+  return {name: name, unit: unit, reserved:reserved, gradReq: gradReq};
+}
+
+const rows = [
+  createData('cs61a', '4', 'yes', 'yes'),
+  createData('cs61b', '4', 'yes', 'yes'),
+  createData('cs61c', '4', 'yes', 'yes'),
+  createData('cs70', '4', 'yes', 'yes'),
+];
+
+function ClassList() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 300}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Class Name </TableCell>
+            <TableCell align="center">unit</TableCell>
+            <TableCell align="center">reserved</TableCell>
+            <TableCell align="center">gradReq</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="center">{row.unit}</TableCell>
+              <TableCell align="center">{row.reserved}</TableCell>
+              <TableCell align="center">{row.gradReq}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default ClassList;
