e35ec5af2adcd8a412266d4aac947f164cabba0e
Brian <brianzheng927@gmail.com>
2021-10-23 17:40:30 -0700

    footer done

--- frontend/phasetime/src/Footer.js
+++ frontend/phasetime/src/Footer.js
@@ -1,20 +1,22 @@
 import React from "react";
 import "./Footer.css";
+import { Button } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import Link from '@mui/material/Link';
 import { Grid } from "@mui/material";
 
 function Footer() {
   return (
     <div className="main-footer">
       <div className="1stLine">
-        <h4 align="center">Cal Phase Time <hr />
-        <Grid align="center" item xs={8}>
+        <h4 align="center">Cal Phase Time 
+        <Link href="https://github.com/Frederic-Chopin/Cal-Hacks"> <GitHubIcon /> </Link>
+        {/* <a> <Button onclick = {() => {<Link href="https://github.com/Frederic-Chopin/Cal-Hacks"> <GitHubIcon /> </Link>}}> 
             <GitHubIcon />
-        </Grid>
+            </Button> 
+        </a> */}
         </h4>
-      </div>
-      <div className="2ndLine">
-        <p align="center" className="col-sm">
+        <p align="center" >
             &copy;{new Date().getFullYear()} CalHacks 8.0 | All rights reserved
         </p>
       </div>
